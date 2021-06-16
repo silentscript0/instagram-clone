cb41222868661e91fb5632ba1d9c0ae888aca566
TomirisAndarbayeva <andarbayeva.tomiris@gmail.com>
2021-06-17 00:55:52 +0600

    added signup and logout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,71 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import Post from './Post';
-import { db } from './firebase';
+import { auth, db } from './firebase';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import { Button, Input } from '@material-ui/core';
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+  const [modalStyle] = React.useState(getModalStyle);
+
   const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        //user has logged in
+        console.log(authUser);
+          setUser(authUser);
+
+          // if (authUser.displayName) {
+          //   //dont update username
+          // } else {
+          //   //if we just created someone
+          //   return authUser.updateProfile({
+          //     displayName: username,
+          //   });
+          // }
+      } else {
+        // yser has logged out
+        setUser(null);
+      }
+    })
+
+    return () => {
+      //perform cleanup
+      unsubscribe();
+    }
+  }, [user, username]);
+
 
   useEffect(() => {
     db.collection('posts').onSnapshot(snapshot => {
@@ -16,8 +77,60 @@ function App() {
     })
   }, []);
 
+const signUp = (event) => {
+  event.preventDefault();
+  auth.createUserWithEmailAndPassword(email, password)
+  .then((authUser) => {
+   return authUser.user.updateProfile({
+      displayName: username
+    })
+  })
+  .catch((error) => alert(error.message))
+}
+
   return (
-    <div className="app">      
+    <div className="app">
+    <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+      >
+        <div style={modalStyle} className={classes.paper}>
+        <center>
+          <img 
+                  className="app__headerImage"
+                  src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+                  alt=""
+          />
+        </center>
+        <form >
+          <center className="app__signup">
+              <Input 
+                type="text"
+                placeholder="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+
+              <Input 
+                placeholder="email"
+                type="text"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+
+              <Input 
+                placeholder="password"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <Button type="submit" onClick={signUp}>Sign Up</Button>
+            </center>
+        </form>
+            
+        </div>
+      </Modal>      
+
      <div className="app__header">
        <img 
           className="app__headerImage"
@@ -26,6 +139,13 @@ function App() {
         />
      </div>
 
+     {user ? (
+      <Button onClick={() => auth.signOut()}>Logout</Button>
+     ) : (
+       <Button onClick={() => setOpen(true)}>Sign Up</Button>
+     )}
+
+      
       <h1>this is an Instagram clone with React, lets gooo!</h1>
 
       {
